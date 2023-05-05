51566583cc46d9857c55e35367f30dfc93641066
dgnyngz <dgnyngz@gmail.com>
2023-05-05 18:40:11 +0300

    tamamlandı

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Anasayfa from "./comps/Anasayfa";
 import SiparisFormu from "./comps/Siparisformu";
 import "./App.css";
 import Basarili from "./comps/Basarili";
+//route bileşenleriyle siparişformu anasayfa ve basarili bileşenleri render edilir
+//<BrowserRouter> komponenti, tarayıcı adres çubuğundaki URL'leri kullanarak hangi bileşenin render edileceğini belirler.
+//<Switch> bileşeni, URL'yi kontrol eder ve belirtilen URL yoluna uygun bir Route bileşeni render eder.
 
 const App = () => {
   return (
@@ -26,3 +29,4 @@ const App = () => {
 };
 
 export default App;
+//npx cypress open
--- src/comps/Anasayfa.js
+++ src/comps/Anasayfa.js
@@ -10,5 +10,5 @@ export default function Anasayfa() {
         ACIKTIM
       </Link>
     </div>
-  );
+  );  
 }
